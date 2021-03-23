24c31cdbf4ed2b1749948f1d9de0a07062d2f148
Litzy-12 <65592160+Litzy-12@users.noreply.github.com>
2021-03-22 20:07:08 -0600

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
             <a-entity scale=".2 .2 .2">
                <a-entity gltf-model="pez.obj" scale="5 5 5" crossOrigin="anonymous"> 
             </a-entity>
-        <a-marker-camera preset="hiro"></a-marker-camera>
+        <a-marker-camera preset='hiro'></a-marker-camera>
         </a-scene>
     </body>
 </html>

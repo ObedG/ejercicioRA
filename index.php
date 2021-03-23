3900983d4fe0f06d2efa3f8952dfae70b3888e39
Litzy-12 <65592160+Litzy-12@users.noreply.github.com>
2021-03-22 19:52:02 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,15 +3,15 @@
     <head>
         <!-- script de A-Frane version1 1.0.0 -->
         <script src="https://aframe.io/releases/1.0.4/aframe.min.js"></script>
-        <!-- script de AR.JS con soporte de marker + location -->
+        
         <script src="https://jeromeetienne.github.io/AR.js/aframe/build/aframe-ar.js"></script>
     </head>
     <body style="margin : 0px; overflow: hidden;">
         <a-scene embedded arjs>
-            <a-marker preset="hiro">
+            <a-entity>
                <a-entity gltf-model="pez.obj" scale="5 5 5" crossOrigin="anonymous"> 
-            </a-marker>
-        <a-entity-camera></a-entity>
+            </a-entity>
+        <a-entity-camera preset="hiro"></a-entity-camera>
         </a-scene>
     </body>
 </html>

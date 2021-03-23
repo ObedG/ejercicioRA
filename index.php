446a4f99079a5ca3d8f0b01a1831eafbeab01de8
Litzy-12 <65592160+Litzy-12@users.noreply.github.com>
2021-03-22 23:54:28 -0600

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   <body>
     <a-scene embedded arjs>
       <a-entity scale=".2 .2 .2"> 
-        <a-entity gltf-model="mesa.glb" scale="10 10 10" rotation="45 0 180" crossOrigin="anonymous"></a-entity>       
+        <a-entity gltf-model="mesa.glb" scale="10 10 10" rotation="75 0 250" crossOrigin="anonymous"></a-entity>       
         <a-marker-camera preset='hiro'></a-marker-camera>
       </a-entity>
      </a-scene>

fc0c09394003ae74f4d10507a768fa3287a09739
Litzy-12 <65592160+Litzy-12@users.noreply.github.com>
2021-03-22 20:06:29 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 <html>
     <head>
         <!-- script de A-Frane version1 1.0.0 -->
-        <script src="https://aframe.io/releases/1.0.4/aframe.min.js"></script>
+        <script src="https://aframe.io/releases/0.8.0/aframe.min.js"></script>
         
         <script src="https://jeromeetienne.github.io/AR.js/aframe/build/aframe-ar.js"></script>
     </head>
     <body>
         <a-scene embedded arjs>
-            <a-entity>
+            <a-entity scale=".2 .2 .2">
                <a-entity gltf-model="pez.obj" scale="5 5 5" crossOrigin="anonymous"> 
             </a-entity>
         <a-marker-camera preset="hiro"></a-marker-camera>

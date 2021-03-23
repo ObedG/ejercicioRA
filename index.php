4f6f21f85b10d39c9459067fda4367c85d93b39e
ObedG <65927298+ObedG@users.noreply.github.com>
2021-03-22 22:03:00 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,16 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <!-- script de A-Frane version1 1.0.0 -->
-        <script src="https://aframe.io/releases/0.8.0/aframe.min.js"></script>
-        
-        <script src="https://jeromeetienne.github.io/AR.js/aframe/build/aframe-ar.js"></script>
-    </head>
-    
-    <body>
-        <a-scene embedded arjs>
-            <a-entity scale=".2 .2 .2">
-               <a-entity obj-model="pez.obj" scale="3 3 3" crossOrigin="anonymous"> 
-            </a-entity>
+<html>  
+  <head>
+    <script src="https://aframe.io/releases/0.8.0/aframe.min.js"></script> 
+    <script src="https://jeromeetienne.github.io/AR.js/aframe/build/aframe-ar.js"></script>
+  </head>   
+
+  <body>
+    <a-scene embedded arjs>
+      <a-entity scale=".2 .2 .2"> 
+        <a-entity obj-model="pez.obj" scale="3 3 3" crossOrigin="anonymous">
+        </a-entity>
         <a-marker-camera preset='hiro'></a-marker-camera>
         </a-scene>
-    </body>
-</html>
+      </body>
+    </html>

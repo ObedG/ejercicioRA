08526338bd81a9b497e32ad882804597915cc897
ObedG <65927298+ObedG@users.noreply.github.com>
2021-04-20 02:07:38 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
     <script src="https://aframe.io/releases/1.2.0/aframe.min.js"></script>
-    
+
 </head>
 
 <body>
@@ -14,10 +14,10 @@
 
             <a-mixin id="campos" position="0 1 3"></a-mixin>
 
-            <a-asset-item id="sombrero" src="som.glb"> </a-asset-item>
+
         </a-assets>
-        <a-entity id="mod" gltf-model = "sombrero" > </a-entity > 
-        
+        <a-entity id="mod" gltf-model="som.glb"> </a-entity>
+
     </a-scene>
 </body>
 

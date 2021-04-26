87c6b6e5944067a670c8047073f681bbe51760c1
ObedG <65927298+ObedG@users.noreply.github.com>
2021-04-26 01:41:55 -0500

    Prueba penultima

--- index.php
+++ index.php
@@ -5,6 +5,20 @@
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
     <script src="https://aframe.io/releases/1.2.0/aframe.min.js"></script>
     <script src="https://jeromeetienne.github.io/AR.js/aframe/build/aframe-ar.js"></script>
+    <script src="https://www.lokeldigital.com/shared-code/a-frame/onclick.js"></script>
+    <script src="https://unpkg.com/aframe-environment-component@1.1.0/dist/aframe-environment-component.min.js"></script>
+    
+    <script>
+      function posForEl(el) {
+        let pos = el.object3D.position
+        return `${pos.x} ${pos.y} ${pos.z}`
+      }
+
+      function moveTo(thing) {
+        let camRig = document.querySelector('#camera-rig')
+        camRig.setAttribute('animation', `property: position; from: ${posForEl(camRig)};to: ${posForEl(this)};dur: 1000`)
+      }
+    </script>
 </head>
 
 <body>
@@ -32,13 +46,19 @@
         </a-entity>
         
         <a-entity id="mdl" mixin=" clickable bouncy">
-        <a-entity id="mod" gltf-model="casisom2.glb" scale="0.300 0.300 0.300" position="-3 1 0"> </a-entity>
+        <a-entity id="mod" gltf-model="ultimo.glb" scale="0.300 0.300 0.300" position="-3 1 0"> </a-entity>
         </a-entity>
          
        
         <a-entity  id="e2" mixin=" campos clickable">
         <a-entity mixin="red ball bouncy" scale="0.3 0.3 0.3"></a-entity>
         </a-entity>
+        
+        <a-entity id="camera-rig" position="0 1 3">
+        <a-entity camera look-controls position="0 0 4" cursor="ray-origin: mouse" raycaster="objects: [geometry]"></a-entity>
+        </a-entity>
+
+    <a-entity environment></a-entity>
     </a-scene>
 </body>
 

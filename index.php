46e55f3fc8ab46d3ee1d77ecafba41045d43b65f
ObedG <65927298+ObedG@users.noreply.github.com>
2021-04-25 23:37:32 -0500

    Update index.php

--- index.php
+++ index.php
@@ -14,10 +14,18 @@
 
             <a-mixin id="campos" position="0 1 3"></a-mixin>
 
-
+         <a-mixin id="clickable" onclick="moveTo"></a-mixin>
+         <a-mixin id="bouncy"
+         animation__bounceup="property: position; from: 0 0 0; to: 0 1 0; easing: easeInOutQuad; dur: 500; start-events: click, animationcomplete__bouncedown"
+         animation__bouncedown="property: position; from: 0 1 0; to: 0 0 0; easing: easeInOutQuad; dur: 500; start-events: animationcomplete__bounceup"
+         ></a-mixin>    
+            
         </a-assets>
+        <a-entity id="mdl" mixin="campos clickable bouncy">
         <a-entity id="mod" gltf-model="som.glb" scale="0.190 0.190 0.190"> </a-entity>
-
+        </a-entity>
+         
+        
     </a-scene>
 </body>
 

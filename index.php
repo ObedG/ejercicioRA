de35b980d3e564c0d7149a4442032de22b85471f
ObedG <65927298+ObedG@users.noreply.github.com>
2021-04-26 01:54:23 -0500

    Update index.php

--- index.php
+++ index.php
@@ -5,20 +5,7 @@
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
     <script src="https://aframe.io/releases/1.2.0/aframe.min.js"></script>
     <script src="https://jeromeetienne.github.io/AR.js/aframe/build/aframe-ar.js"></script>
-    <script src="https://www.lokeldigital.com/shared-code/a-frame/onclick.js"></script>
-    <script src="https://unpkg.com/aframe-environment-component@1.1.0/dist/aframe-environment-component.min.js"></script>
     
-    <script>
-      function posForEl(el) {
-        let pos = el.object3D.position
-        return `${pos.x} ${pos.y} ${pos.z}`
-      }
-
-      function moveTo(thing) {
-        let camRig = document.querySelector('#camera-rig')
-        camRig.setAttribute('animation', `property: position; from: ${posForEl(camRig)};to: ${posForEl(this)};dur: 1000`)
-      }
-    </script>
 </head>
 
 <body>
@@ -54,11 +41,7 @@
         <a-entity mixin="red ball bouncy" scale="0.3 0.3 0.3"></a-entity>
         </a-entity>
         
-        <a-entity id="camera-rig" position="0 1 3">
-        <a-entity camera look-controls position="0 0 4" cursor="ray-origin: mouse" raycaster="objects: [geometry]"></a-entity>
-        </a-entity>
-
-    <a-entity environment></a-entity>
+        
     </a-scene>
 </body>
 
